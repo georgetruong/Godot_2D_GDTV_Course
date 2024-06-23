6f66dc6738beaf82afb78fae72c9cd3b60ab50d2
George Truong <truong.george@gmail.com>
2024-06-23 13:27:36 -0700

    Clamp players position

--- 003-AlienAttack/scripts/player.gd
+++ 003-AlienAttack/scripts/player.gd
@@ -4,6 +4,7 @@ var speed = 300
 
 func _physics_process(delta):
 	velocity = Vector2(0, 0)
+
 	if Input.is_action_pressed("move_right"):
 		velocity.x = speed
 	if Input.is_action_pressed("move_left"):
@@ -12,4 +13,8 @@ func _physics_process(delta):
 		velocity.y = -speed
 	if Input.is_action_pressed("move_down"):
 		velocity.y = speed
+
 	move_and_slide()
+
+	# Prevent player from moving beyond the screen
+	global_position = global_position.clamp(Vector2(0, 0), get_viewport_rect().size)
