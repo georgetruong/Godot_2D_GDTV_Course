ff6ace02c5b179ea54b334b2f8e49e3bf69fb5af
George Truong <truong.george@gmail.com>
2024-06-24 09:26:03 -0700

    New project setup

---
+++ 004-MartianMike/scripts/level.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+func _ready():
+	pass 
+
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
