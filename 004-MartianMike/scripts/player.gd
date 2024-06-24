2070f3e55faf770656d7874eee5164e16ef7c7a2
George Truong <truong.george@gmail.com>
2024-06-24 10:20:59 -0700

    Update player animations and add double jump ability

--- 004-MartianMike/scripts/player.gd
+++ 004-MartianMike/scripts/player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+var current_jumps = 0
+var max_jumps = 2
+
 @export var gravity = 400
 @export var jump_force = 12500
 @export var speed = 7500
@@ -14,11 +17,30 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 		if velocity.y > 500:
 			velocity.y = 500
+	else:
+		current_jumps = 0
 
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") && current_jumps < max_jumps: #&& is_on_floor():
 		velocity.y = -jump_force * delta
+		current_jumps += 1
 
 	var direction = Input.get_axis("move_left", "move_right")
 	velocity.x = direction * speed * delta
 
 	move_and_slide()
+	update_animations(direction)
+
+func update_animations(direction):
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
+
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
