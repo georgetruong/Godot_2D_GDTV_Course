1c6d64831e7e0b72323b444f1d767deb4e14590c
George Truong <truong.george@gmail.com>
2024-06-26 11:10:40 -0700

    Add more terrain and create tutorial level

--- 004-MartianMike/scripts/hud.gd
+++ 004-MartianMike/scripts/hud.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func set_time_label(value):
-    $TimeLabel.text = "TIME: " + str(value)
+	$TimeLabel.text = "TIME: " + str(value)
--- 004-MartianMike/scripts/ui_layer.gd
+++ 004-MartianMike/scripts/ui_layer.gd
@@ -1,4 +1,4 @@
 extends CanvasLayer
 
 func show_win_screen(flag: bool):
-    $WinScreen.visible = flag
+	$WinScreen.visible = flag
