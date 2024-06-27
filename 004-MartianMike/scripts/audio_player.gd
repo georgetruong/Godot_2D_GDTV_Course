4850ecd49265e15563978b2dae594605f5e0526b
George Truong <truong.george@gmail.com>
2024-06-27 15:43:40 -0700

    Finish level 3 and play escape music when player wins

--- 004-MartianMike/scripts/audio_player.gd
+++ 004-MartianMike/scripts/audio_player.gd
@@ -2,12 +2,12 @@ extends Node
 
 var hurt = preload("res://assets/audio/hurt.wav")
 var jump = preload("res://assets/audio/jump.wav")
-var mute_all: bool = true
-var sfx_volume_db = 0
+var mute_all: bool = false
+var master_volume_db = 0
 
 func _ready():
-	$MusicPlayer.volume_db = -100
-	sfx_volume_db = -100
+	$MusicPlayer.volume_db = 0
+	master_volume_db = 0
 
 func play_sfx(sfx_name: String):
 	var stream = null 
@@ -23,7 +23,7 @@ func play_sfx(sfx_name: String):
 	asp.name = "SFX"
 	asp.stream = stream
 	add_child(asp)
-	asp.volume_db = sfx_volume_db
+	asp.volume_db = master_volume_db
 	asp.play()
 	await asp.finished
 	asp.queue_free()
@@ -32,7 +32,23 @@ func toggle_mute():
 	mute_all = !mute_all
 	if mute_all:
 		$MusicPlayer.volume_db = -100
-		sfx_volume_db = -100
+		master_volume_db = -100
 	else:
 		$MusicPlayer.volume_db = 0
-		sfx_volume_db = 0
+		master_volume_db = 0
+
+func play_win_music():
+	play_music(load("res://assets/audio/music/Escaping.WAV"))
+
+func play_bg_music():
+	play_music(load("res://assets/audio/music/MUSIC.WAV"))
+
+func play_music(stream):
+	if stream:
+		$MusicPlayer.stop()
+		$MusicPlayer.volume_db = master_volume_db
+		$MusicPlayer.stream = stream
+		$MusicPlayer.play()
+	else:
+		print("Invalid music file")
+	
--- 004-MartianMike/scripts/win_screen.gd
+++ 004-MartianMike/scripts/win_screen.gd
@@ -1,4 +1,8 @@
 extends Control
 
+func _ready():
+	AudioPlayer.play_win_music()
+
 func _on_button_pressed():
+	AudioPlayer.play_bg_music()
 	get_tree().change_scene_to_file("res://scenes/start_menu.tscn")
